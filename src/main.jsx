a0f2783e533505e41f5f3209bcecd94ca0c314a2
Irfan Saeed <irfansaeed.narejo@gmail.com>
2026-03-08 18:58:13 +0500

    Refactor code structure for improved readability and maintainability

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import './index.css'
 import IntroTOJetpack from './IntroTOJetpack.jsx'
 import StateManagement from './StateManagement.jsx'
 import Navigation from './Navigation.jsx'
-
+import New from './Newss.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <IntroTOJetpack />
-    <StateManagement />
-    <Navigation />
-
+    <New/>
+    <IntroTOJetpack/>
+    <StateManagement/>
+    <Navigation/>
     
   </StrictMode>,
 )
