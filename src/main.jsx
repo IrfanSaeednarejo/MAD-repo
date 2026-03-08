c0597f32a2f5c358a00fb307e7a07585309daf9c
Irfan Saeed <irfansaeed.narejo@gmail.com>
2026-03-08 19:32:21 +0500

    Remove unused component 'New' from main rendering in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import './index.css'
 import IntroTOJetpack from './IntroTOJetpack.jsx'
 import StateManagement from './StateManagement.jsx'
 import Navigation from './Navigation.jsx'
-import New from './Newss.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <New/>
     <IntroTOJetpack/>
     <StateManagement/>
     <Navigation/>
